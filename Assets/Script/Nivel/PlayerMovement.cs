7fe4a432b3e1cf3aff062b14627b6b924499dee2
RomanMartinez <roman.martinezcristaldo@gmail.com>
2022-05-07 14:40:36 -0300

    Animator personaje

--- Assets/Script/Nivel/PlayerMovement.cs
+++ Assets/Script/Nivel/PlayerMovement.cs
@@ -6,15 +6,20 @@ using UnityEngine.SceneManagement;
 public class PlayerMovement : MonoBehaviour
 {
     bool alive = true;
-
     public float Speed = 5;
     public Rigidbody rb;
 
     float horizontalInput;
     public float horizontalMultiplier = 2;
 
+    Animator Animator;
+    int IsDead;
 
-
+    private void Start()
+    {
+        Animator = GetComponent<Animator>();
+        IsDead = Animator.StringToHash("IsDead");
+    }
     private void FixedUpdate()
     {
         if (!alive) return;
@@ -22,6 +27,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 forwardMove = transform.forward * Speed * Time.fixedDeltaTime;
         Vector3 horizontalMove = transform.right * horizontalInput * Speed * Time.fixedDeltaTime * horizontalMultiplier;
         rb.MovePosition(rb.position + forwardMove + horizontalMove);
+        Animator.SetBool("IsDead", false);
     }
     private void Update()
     {
@@ -31,6 +37,9 @@ public class PlayerMovement : MonoBehaviour
 
     public void Die()
     {
+        Animator.SetBool("IsDead", true);
+        Object.Destroy(rb);
+        Speed = 0;
         alive = false;
         Invoke("Restart", 2);
     }
