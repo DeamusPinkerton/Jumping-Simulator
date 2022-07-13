c6d6f4d9f6453e87b383933d2740428366439b06
RomanMartinez <roman.martinezcristaldo@gmail.com>
2022-07-13 16:24:14 -0300

    a tu nombre señor *se muere

--- Assets/Script/Nivel/ColectableEntity.cs
+++ Assets/Script/Nivel/ColectableEntity.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColectableEntity : MonoBehaviour //TPFinal - Roman A Martinez Cristaldo
+public class ColectableEntity : MonoBehaviour //TPFinal - Juan Manuel Calace
 {
     public virtual void Collectable(int type)
     {
